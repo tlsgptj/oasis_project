824358d6db529268891b1e1d26373d6d1cd5b155
Da-Yeon-Kim <210941@jnu.ac.kr>
2024-08-25 03:39:23 +0900

    feat: 카카오맵 API 사용 (웹 지원 X)

--- lib/src/screens/map_screen.dart
+++ lib/src/screens/map_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 import '../widgets/custom_navigation_bar.dart';
 import '../widgets/custom_end_drawer.dart';
 
@@ -11,19 +13,37 @@ class MapScreen extends StatefulWidget {
 
 class MapScreenState extends State<MapScreen> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  WebViewController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    if (!kIsWeb) {
+      // Android 또는 iOS에서 WebView 초기화
+      _controller = WebViewController()
+        ..setJavaScriptMode(JavaScriptMode.unrestricted)
+        ..loadFlutterAsset('assets/kakao_map.html');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
-      endDrawer: const CustomEndDrawer(), // 햄버거 메뉴와 연결된 Drawer
-      body: const Center(
-        child: Text('지도 화면'),
-      ),
+      endDrawer: const CustomEndDrawer(),
+      body: kIsWeb
+          ? const Center(
+              child: Text('WebView is not supported on web'),
+            )
+          : (_controller != null
+              ? WebViewWidget(controller: _controller!)
+              : const Center(
+                  child: Text('This platform does not support WebView'),
+                )),
       bottomNavigationBar: CustomNavigationBar(
-        selectedIndex: null, // 아무것도 선택되지 않은 상태로 유지
+        selectedIndex: null,
         scaffoldKey: _scaffoldKey,
-        isMapScreen: true, // 지도 화면이므로 true
+        isMapScreen: true,
       ),
     );
   }
