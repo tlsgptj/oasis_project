e676ae1df5907e869fca10f0492b94f2a7d1b6ba
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-08-24 16:56:18 +0900

    내 리뷰만 보기 기능이랑 모든 리뷰 조회 기능이랑 나눴어용

--- lib/src/screens/all_reviews_screen.dart
+++ lib/src/screens/all_reviews_screen.dart
@@ -1,31 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import '../services/auth_service.dart';
 
-class MyReviewsScreen extends StatefulWidget {
-  const MyReviewsScreen({super.key});
+class AllReviewsScreen extends StatefulWidget {
+  final AuthService authService;
+
+  const AllReviewsScreen({super.key, required this.authService});
 
   @override
-  _MyReviewsScreenState createState() => _MyReviewsScreenState();
+  _AllReviewsScreenState createState() => _AllReviewsScreenState();
 }
 
-class _MyReviewsScreenState extends State<MyReviewsScreen> {
+class _AllReviewsScreenState extends State<AllReviewsScreen> {
   late Future<List<Review>> _reviews;
 
   @override
   void initState() {
     super.initState();
-    _reviews = fetchReviews();
+    _reviews = fetchAllReviews();
   }
 
-  Future<List<Review>> fetchReviews() async {
-    final response = await http.get(Uri.parse('http://localhost:8000/my-reviews/'));
+  Future<List<Review>> fetchAllReviews() async {
+    final token = await widget.authService.storage.read(key: 'jwt');
+
+    if (token == null) {
+      throw Exception('토큰이 없습니다. 로그인이 필요합니다.');
+    }
+
+    final response = await http.get(
+      Uri.parse('http://localhost:8000/all-reviews/'),
+      headers: {
+        'Authorization': 'Bearer $token',
+      },
+    );
 
     if (response.statusCode == 200) {
       final List<dynamic> data = json.decode(response.body);
       return data.map((json) => Review.fromJson(json)).toList();
     } else {
-      throw Exception('Failed to load reviews');
+      throw Exception('Failed to load all reviews');
     }
   }
 
@@ -33,7 +47,7 @@ class _MyReviewsScreenState extends State<MyReviewsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('내 리뷰'),
+        title: const Text('모든 리뷰'),
       ),
       body: FutureBuilder<List<Review>>(
         future: _reviews,
@@ -86,3 +100,4 @@ class Review {
     );
   }
 }
+
