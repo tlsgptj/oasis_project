4d78aee13da4cfaafbafa08fd9bc8c409a37f579
Da-Yeon-Kim <210941@jnu.ac.kr>
2024-08-26 20:24:52 +0900

    design: 로그인/회원가입 페이지 스타일링

--- lib/src/screens/register_screen.dart
+++ lib/src/screens/register_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '../services/auth_service.dart';
-import '../widgets/custom_text_field.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -53,46 +52,177 @@ class RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white, // 전체 배경색을 하얀색으로 설정
       appBar: AppBar(
-        title: const Text('Sign Up'),
+        title: const Text('회원가입'),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Color(0xFFFDB132)),
+        titleTextStyle: const TextStyle(
+          color: Color(0xFFFDB132),
+          fontSize: 20,
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            CustomTextField(
-              controller: _nicknameController,
-              labelText: 'Nickname',
-            ),
-            const SizedBox(height: 16),
-            CustomTextField(
-              controller: _emailController,
-              labelText: 'Email',
-            ),
-            const SizedBox(height: 16),
-            CustomTextField(
-              controller: _passwordController,
-              labelText: 'Password',
-              obscureText: true,
-            ),
-            const SizedBox(height: 16),
-            CustomTextField(
-              controller: _confirmPasswordController,
-              labelText: 'Confirm Password',
-              obscureText: true,
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _register,
-              child: const Text('Sign Up'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/login');
-              },
-              child: const Text('Sign In'),
-            ),
-          ],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              // 이름 입력 필드
+              ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 300),
+                child: TextField(
+                  controller: _nicknameController,
+                  decoration: const InputDecoration(
+                    labelText: '이름',
+                    filled: true,
+                    fillColor: Color(0xFFF8E6C7),
+                    prefixIcon: Icon(Icons.person, color: Color(0xFFFDB132)),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide: BorderSide(color: Color(0xFFFDB132)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide: BorderSide(color: Color(0xFFFDB132)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide:
+                          BorderSide(color: Color(0xFFFDB132), width: 2.0),
+                    ),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              // 이메일 입력 필드
+              ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 300),
+                child: TextField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    labelText: '이메일',
+                    filled: true,
+                    fillColor: Color(0xFFF8E6C7),
+                    prefixIcon: Icon(Icons.email, color: Color(0xFFFDB132)),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide: BorderSide(color: Color(0xFFFDB132)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide: BorderSide(color: Color(0xFFFDB132)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide:
+                          BorderSide(color: Color(0xFFFDB132), width: 2.0),
+                    ),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              // 비밀번호 입력 필드
+              ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 300),
+                child: TextField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: '비밀번호',
+                    filled: true,
+                    fillColor: Color(0xFFF8E6C7),
+                    prefixIcon: Icon(Icons.lock, color: Color(0xFFFDB132)),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide: BorderSide(color: Color(0xFFFDB132)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide: BorderSide(color: Color(0xFFFDB132)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide:
+                          BorderSide(color: Color(0xFFFDB132), width: 2.0),
+                    ),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              // 비밀번호 확인 입력 필드
+              ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 300),
+                child: TextField(
+                  controller: _confirmPasswordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: '비밀번호 확인',
+                    filled: true,
+                    fillColor: Color(0xFFF8E6C7),
+                    prefixIcon:
+                        Icon(Icons.lock_outline, color: Color(0xFFFDB132)),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide: BorderSide(color: Color(0xFFFDB132)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide: BorderSide(color: Color(0xFFFDB132)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide:
+                          BorderSide(color: Color(0xFFFDB132), width: 2.0),
+                    ),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 24),
+              // 회원가입 버튼
+              ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 300),
+                child: ElevatedButton(
+                  onPressed: _register,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFFDB132),
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    minimumSize:
+                        const Size(double.infinity, 50), // 버튼 크기를 텍스트 필드와 맞춤
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  child:
+                      const Text('회원가입', style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              const SizedBox(height: 16),
+              // 로그인 버튼
+              ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 300),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/login');
+                  },
+                  child: const Text('로그인',
+                      style: TextStyle(
+                          color: Color(0xFFFDB132),
+                          decoration: TextDecoration.underline)),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/src/widgets/custom_text_field.dart
+++ lib/src/widgets/custom_text_field.dart
@@ -1,26 +1,194 @@
 import 'package:flutter/material.dart';
+import '../services/auth_service.dart';
 
-class CustomTextField extends StatelessWidget {
-  final TextEditingController controller;
-  final String labelText;
-  final bool obscureText;
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
 
-  const CustomTextField({
-    super.key,
-    required this.controller,
-    required this.labelText,
-    this.obscureText = false,
-  });
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final AuthService _authService = AuthService();
+
+  void _signIn() async {
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    if (email.isNotEmpty && password.isNotEmpty) {
+      bool success = await _authService.login(email, password);
+      if (!mounted) return;
+      if (success) {
+        Navigator.pushNamed(context, '/home');
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('로그인 실패')),
+        );
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('모든 필드를 입력하세요')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: controller,
-      decoration: InputDecoration(
-        labelText: labelText,
-        border: const OutlineInputBorder(),
+    return Scaffold(
+      backgroundColor: Colors.white, // 배경색을 흰색으로 설정
+      appBar: AppBar(
+        title: const Text('로그인'),
+        backgroundColor: Colors.white, // AppBar의 배경색도 흰색으로 설정
+        elevation: 0, // 그림자 제거
+        iconTheme: const IconThemeData(color: Color(0xFFFDB132)), // 아이콘 색상 설정
+        titleTextStyle: const TextStyle(
+          color: Color(0xFFFDB132),
+          fontSize: 20,
+        ), // 제목 스타일 설정
       ),
-      obscureText: obscureText,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, // 중앙 정렬
+            crossAxisAlignment: CrossAxisAlignment.center, // 중앙 정렬
+            children: <Widget>[
+              // 이메일 입력 필드
+              ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 300),
+                child: TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    labelText: '이메일',
+                    filled: true,
+                    fillColor: const Color(0xFFF8E6C7), // 배경색 설정
+                    prefixIcon: const Icon(Icons.email,
+                        color: Color(0xFFFDB132)), // 아이콘 설정
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: const BorderSide(
+                          color: Color(0xFFFDB132)), // 기본 테두리 색상
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: const BorderSide(
+                          color: Color(0xFFFDB132)), // 비활성화 상태 테두리 색상
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: const BorderSide(
+                          color: Color(0xFFFDB132),
+                          width: 2.0), // 포커스 상태 테두리 색상
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              // 비밀번호 입력 필드
+              ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 300),
+                child: TextField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: '비밀번호',
+                    filled: true,
+                    fillColor: const Color(0xFFF8E6C7), // 배경색 설정
+                    prefixIcon: const Icon(Icons.lock,
+                        color: Color(0xFFFDB132)), // 아이콘 설정
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: const BorderSide(
+                          color: Color(0xFFFDB132)), // 기본 테두리 색상
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: const BorderSide(
+                          color: Color(0xFFFDB132)), // 비활성화 상태 테두리 색상
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: const BorderSide(
+                          color: Color(0xFFFDB132),
+                          width: 2.0), // 포커스 상태 테두리 색상
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 24),
+              // 로그인 버튼
+              ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 300),
+                child: ElevatedButton(
+                  onPressed: _signIn,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFFDB132), // 버튼 색상 설정
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 16), // 버튼 패딩 설정
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30), // 버튼 둥근 모서리
+                    ),
+                  ),
+                  child: const Text('로그인',
+                      style: TextStyle(color: Colors.white)), // 버튼 텍스트 스타일
+                ),
+              ),
+              const SizedBox(height: 16),
+              // OR 텍스트와 Divider
+              Row(
+                children: [
+                  const Expanded(
+                    child: Divider(
+                      color: Colors.grey,
+                      thickness: 1,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text(
+                      'OR',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                  const Expanded(
+                    child: Divider(
+                      color: Colors.grey,
+                      thickness: 1,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              // 회원가입 버튼
+              ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 300),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/register');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFFDB132), // 버튼 색상 설정
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 16), // 버튼 패딩 설정
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30), // 버튼 둥근 모서리
+                    ),
+                  ),
+                  child: const Text('회원가입',
+                      style: TextStyle(color: Colors.white)), // 버튼 텍스트 스타일
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
